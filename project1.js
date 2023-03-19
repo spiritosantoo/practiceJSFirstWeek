da9ed5072fb1a770510796d94e61df20a336d7d4
spiritosantoo <allashlapak5@gmail.com>
2023-03-19 22:54:00 +0200

    practiceJSFirstWeek(2)

--- project1.js
+++ project1.js
@@ -1,17 +1,36 @@
-const numberOfFilms = +prompt('Скільки фільмів ви переглянули?', 'Уведіть кількість')
+function back(str) {
+   let stopper = true
+   while (stopper) {
+      if (str === null || str.length <= 0 || str.length > 50) {
+         str = prompt('Помилка! Уведіть ще раз', '')
+      } else stopper = false
+   }
+   return str
+}
+
+let numberOfFilms = back(prompt('Скільки фільмів ви переглянули?', ''))
 
 const personalMovieDB = {
-   count: numberOfFilms,
+   count: +numberOfFilms,
    movies: {},
    actors: {},
    genres: [],
    privat: false
 }
 
-const a = prompt('Один із останніх переглянутих фільмів?', 'Уведіть назву фільму'),
-      b = prompt('На скільки оціните його?', 'Уведіть свою оцінку'),
-      c = prompt('Один із останніх переглянутих фільмів?', 'Уведіть назву фільму'),
-      d = prompt('На скільки оціните його?', 'Уведіть свою оцінку')
+for (let i = 0; i < 2; i++) {
+   let a = back(prompt('Один із останніх переглянутих фільмів?', ''))
+   let b = back(prompt('Уведіть оцінку', ''))
+   personalMovieDB.movies[a] = b
+}
+console.log(personalMovieDB.movies)
 
-personalMovieDB.movies[a] = b
-personalMovieDB.movies[c] = d
+if (personalMovieDB.count < 10) {
+   console.log('Переглянуто доволі мало фільмів')
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+   console.log('Ви любите смокінги')
+} else if (personalMovieDB.count >= 30) {
+   console.log('Та ви, нахуй, кіноман!')
+} else {
+   console.log('Відбулась помилка природи')
+}
